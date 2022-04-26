8392d2083714e766bec5c5a4ac2b3a25c10008c6
fdetsch <fdetsch@web.de>
2022-04-26 19:58:23 +0200

    uses wkt to avoid `sp::CRS()` related errors (#5)

--- R/gsodDf2Sp.R
+++ R/gsodDf2Sp.R
@@ -25,6 +25,8 @@
 #'                          
 #' plot(gsod_shp)                        
 #' 
+#' @importFrom sf st_as_sf
+#' 
 #' @export gsodDf2Sp
 #' @aliases gsodDf2Sp
 gsodDf2Sp <- function(data, 
@@ -33,8 +35,16 @@ gsodDf2Sp <- function(data,
   # Subset data by valid coordinates and convert to SpatialPointsDataFrame
   data.lonlat <- subset(data, !is.na(LON) & !is.na(LAT))
   
-  coordinates(data.lonlat) <- ~ LON + LAT
-  proj4string(data.lonlat) <- CRS("+init=epsg:4326")
+  data.lonlat = sf::st_as_sf(
+    data.lonlat
+    , coords = c("LON", "LAT")
+    , crs = 4326
+  )
   
-  return(data.lonlat)
+  return(
+    as(
+      data.lonlat
+      , Class = "Spatial"
+    )
+  )
 }
